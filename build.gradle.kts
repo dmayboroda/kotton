cb22bfb78c52d9184346783eb74b25f6ccfb1616
David Mayboroda <david.ftzi@gmail.com>
2017-05-17 17:48:21 +0300

    gradle kotlin scripting build

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        maven { setUrl("https://repo.spring.io/milestone") }
+        maven { setUrl("https://repo.spring.io/snapshot") }
+    }
+
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.BUILD-SNAPSHOT")
+    }
+}
+
+plugins {
+    val kotlinVersion = "1.1.2-2"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
+}
+
+apply {
+    plugin("org.springframework.boot")
+}
+
+group = "com.dmayboroda"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { setUrl("https://repo.spring.io/milestone") }
+    maven { setUrl("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
+    compile("org.jetbrains.kotlin:kotlin-reflect")
+    compile("org.springframework:spring-webflux")
+    compile("io.projectreactor:reactor-kotlin-extensions:1.0.0.M2")
+
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin")
+    compile("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
+    testCompile("io.projectreactor.addons:reactor-test")
+    testCompile("org.springframework.boot:spring-boot-starter-test")
+
+}
